615dcc7a3ebd995d78478fbe4c13a5112e235a67
romain1.vidal <romain1.vidal@epita.fr>
2025-06-08 09:12:04 +0200

    Init proj

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>Ozone92 Page</header>
+      <ul>
+        <li>
+        <a href='../Jean-Tombe/index.html'>Jean-Tombe site</a>
+        </li>
+
+        <li>
+        <a href='https://ozone92.itch.io'>My itch.io</a>
+        </li>
+
+        <li>
+        <a href='https://github.com/Ozone92'>My github</a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default App
